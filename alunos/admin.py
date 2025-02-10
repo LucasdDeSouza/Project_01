7e64a67c2d394d195c40fb2bcfb36be62c255645
lucas <lucasdesouzasouza57@gmail.com>
2025-02-10 11:37:44 -0300

    Parte 2

--- alunos/admin.py
+++ alunos/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Estado
+from .models import Cidade
+from .models import Pessoa
 
 admin.site.register(Estado)
+admin.site.register(Cidade)
+admin.site.register(Pessoa)
--- alunos/models.py
+++ alunos/models.py
@@ -5,5 +5,22 @@ class Estado(models.Model):
     nome = models.CharField(max_length=50)
     sigla = models.CharField(max_length=2)
 
-    def _str_(self):
-        return self.nome
+    def __str__(self):
+        return f"{self.nome} ({self.sigla})"
+
+
+class Cidade(models.Model):
+    nome = models.CharField(max_length=50)
+    estado = models.ForeignKey(Estado, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.nome} ({self.estado.sigla})"
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=50)
+    telefone = models.CharField(max_length=13)
+    email = models.EmailField(max_length=50)
+    data = models.DateField(max_length=10)
+    cidade = models.ForeignKey(Cidade, on_delete=models.PROTECT)
+    def __str__(self):
+        return f"{self.nome} ({self.cidade})"
